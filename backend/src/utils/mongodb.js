8abc997e7031f7acb6bdf9965eb01bf813b6e130
xuerongNanopay <xuerong@nanopay.net>
2023-07-02 14:43:34 -0400

    refactor mongodb util

--- backend/src/utils/mongodb.js
+++ backend/src/utils/mongodb.js
@@ -4,17 +4,16 @@ const MongoClient = mongodb.MongoClient;
 const url = 'mongodb://root:123456@localhost:27017';
 
 let _db;
-const mongoConnect = _ => {
-  return MongoClient.connect(url)
-  .then(client => {
-    console.log('mongodb collect')
-    _db = client.db('nanopay');
-    return _db;
-  })
-  .catch(err => {
-    console.log(err)
-    throw err;
-  });
+const mongoConnect = async (database = 'nanopay') => {
+
+  try {
+    const client = await MongoClient.connect(url);
+    console.log('Connect to MongoDb');
+    _db = await client.db(database);
+    console.log('Connect to database: ' + database);
+  } catch ( err ) {
+    console.log(err);
+  }
 }
 
 const getDb = () => {
