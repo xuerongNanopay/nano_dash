3746e1bf48dc0c21347c921f9bfe3c8c554bc510
xuerongNanopay <xuerong@nanopay.net>
2023-07-08 19:11:31 -0400

    bug fix

--- backend/script/dump-analytic-events-to-file.js
+++ backend/script/dump-analytic-events-to-file.js
@@ -7,20 +7,19 @@ const folderPath = path.join(__dirname, '../..', 'files');
 const dumpAnalyticEventToFile = async (token, start, end) => {
   const fileName = 'analytic_event_' + start + '_' + end + '.txt';
   const filePath = path.join(folderPath, fileName)
-  // if ( fs.existsSync(filePath) ) {
-  //   console.warn('[Warn] fileName: ' + filePath + ' already exists')
-  //   return;
-  // }
+  if ( fs.existsSync(filePath) ) {
+    console.warn('[Warn] fileName: ' + filePath + ' already exists')
+    return;
+  }
   let events = await pullAnalyticEvent(token, start, end);
   for ( let e of events ) {
     const formattedDate = new Date(e.timestamp.replace('Z', '+00:00'));
     e.timestamp = formattedDate.getTime();
     e.createdAt = formattedDate;
   }
-  console.log(events);
-  // for ( let e of events ) {
-  //   fs.appendFileSync(filePath, JSON.stringify(e) + '\n');
-  // }
+  for ( let e of events ) {
+    fs.appendFileSync(filePath, JSON.stringify(e) + '\n');
+  }
 }
 
 
