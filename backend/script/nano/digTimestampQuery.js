c0083803f397b6499fa0f7e3c2d0e197681efc47
xuerongNanopay <xuerong@nanopay.net>
2023-07-08 13:41:05 -0400

    add helper for parsing time from TZ: UTC and Toronto

--- backend/script/nano/digTimestampQuery.js
+++ backend/script/nano/digTimestampQuery.js
@@ -1,4 +1,5 @@
 const { formatInTimeZone } = require('date-fns-tz');
+const { format } = require('date-fns');
 const { 
   startOfToday ,
   endOfToday,
@@ -8,13 +9,18 @@ const {
   endOfMonth,
   startOfQuarter,
   endOfQuarter,
+  startOfYesterday,
   startOfYear,
   endOfYear,
-  addMinutes
+  addMinutes,
+  subMonths,
+  subDays,
+  subWeeks,
+  parseISO
 } = require('date-fns');
 
-const winDate = new Date('2023-06-30T03:00:00')
-console.log(winDate.toUTCString())
+const winDate = new Date('2023-06-30T03:00' + '+00:00')
+console.log(format(winDate, "yyyy-MM-dd'T'HH'%3A'mm"))
 console.log(formatInTimeZone(winDate, 'Etc/Greenwich', "yyyy-MM-dd'T'HH'%3A'mm"))
 console.log('---')
 
@@ -23,26 +29,47 @@ function toUTCDigFormat(date) {
   return formatInTimeZone(date, 'Etc/Greenwich', _digTimeFormat);
 }
 
-function getDigRange(start, end) {
+function getUTCDigRange(start, end) {
   console.log(start, end)
   return [toUTCDigFormat(start), toUTCDigFormat(addMinutes(end, 1))];
 }
 
-const getTimeRange = (option) => {
+//startTime and endTime will be treaded as UTC time
+//eg: 2023-06-30T03:00
+function convertToDigRangeFromUTCTime(startTime, endTime) {
+  const start = new Date(startTime + '+00:00');
+  const end = new Date(endTime + '+00:00');
+  return [toUTCDigFormat(start), toUTCDigFormat(end)];
+}
+
+//The date return by this function is based on UTC of EST/PST(Toronto)
+const getDigTimeRangWithTZToronto = (option) => {
   switch(option) {
     case 'thisDay':
       return getDigRange(startOfToday(), endOfToday());
+    case 'thisTwoDay':
+      return getDigRange(startOfYesterday(), endOfToday())
     case 'thisWeek':
-      return getDigRange(startOfWeek(), endOfWeek());
+      return getDigRange(startOfWeek(startOfToday()), endOfWeek(startOfToday()));
     case 'thisMonth':
-      return getDigRange(startOfMonth(), endOfMonth());
+      return getDigRange(startOfMonth(startOfToday()), endOfMonth(startOfToday()));
+    case 'thisTwoMonth':
+      return getDigRange(subMonths(startOfMonth(startOfToday()), 1), endOfMonth(startOfToday()));
     case 'thisQuarter':
-      return getDigRange(startOfQuarter(), endOfQuarter());
-    default: 
-      return ['start', 'end'];
-  }
+      return getDigRange(startOfQuarter(startOfToday()), endOfQuarter(startOfToday()));
+    case 'thisYear':
+      return getDigRange(startOfYear(startOfToday()), endOfYear(startOfToday()));
+    default:
+      const [startTime, endTime] = option.split("/");
+      //TODO: health check.
+      return [toUTCDigFormat(new Date(startTime)), toUTCDigFormat(new Date(endTime))]
+    }
 }
 
-console.log('aaa')
-const ret = getTimeRange('thisDay');
-console.log(ret)
+// getDigTimeRangWithTZToronto('2023-06-30T00:00/2023-06-30T04:00');
+// console.log(convertToDigRangeFromUTCTime('2023-06-30T03:41', '2023-06-30T04:00'));
+
+module.exports = {
+  getDigTimeRangWithTZToronto,
+  convertToDigRangeFromUTCTime
+}
